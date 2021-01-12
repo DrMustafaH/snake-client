802e4c4a89473edb85cb15d68ae0cf104b2f893c
DrMustafaH <mustafaharith@gmail.com>
2021-01-12 12:51:16 -0800

    make the snake move by wasd keys

--- client.js
+++ client.js
@@ -7,19 +7,15 @@ const connect = function () {
     port: 50541
   });
   conn.on("connect", () => console.log("Successfully connected to game server")),
-    setTimeout(() => console.log("you ded cuz you idled"), MAX_IDLE_TIMEOUT)
+    conn.on("data", (data) => console.log(data))
   conn.on('connect', () => {
     conn.write('Name: ABC');
-    // setInterval(() => conn.write('Move: up'), 2000);
-    // setTimeout(() => conn.write('Move: right'), 2000);
-    // setTimeout(() => conn.write('Move: down'), 3000);
-    // setTimeout(() => conn.write('Move: down'), 4000);
-    // conn.write('Move: right');
-    // conn.write('Move: down');
-    // conn.write('Move: left');
+    conn.on('end', () => {
+      console.log("Connection End");
+    });
+
+
   });
-  // conn.on('connect', () => {
-  // });
   conn.setEncoding('utf8');
   return conn;
 }
--- input.js
+++ input.js
@@ -1,4 +1,10 @@
-const setupInput = function () {
+
+
+let connection;
+
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -13,8 +19,22 @@ const handleUserInput = function () {
     if (key === '\u0003') {
       process.exit();
     }
+    if (key === 'w') {
+      connection.write('Move: up')
+    } else if (key === 'a') {
+      connection.write('Move: left')
+    } else if (key === 'd') {
+      connection.write('Move: right')
+    } else if (key === 's') {
+      connection.write('Move: down')
+    }
   });
 }
+
+// conn.write('Move: right');
+// ;
+// conn.write('Move: down');
+// ;
 
 module.exports = {
   setupInput
